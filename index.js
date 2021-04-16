1200e2d3ee1d2200ee02e24792eed4bd372588e2
keerthi538 <keerthikeerthik275@gmail.com>
2021-04-16 22:03:52 +0530

    class 16/04/2021

--- index.js
+++ index.js
@@ -1,24 +1,66 @@
 
-const formSubmit = (e) => {
+// const formSubmit = (e) => {
 
-    const name = document.getElementById('name');
-    const email = document.getElementById('email');
-    console.log(name.value + " 's mail address is " + email.value);
-}
+//     const name = document.getElementById('name');
+//     const email = document.getElementById('email');
+//     console.log(name.value + " 's mail address is " + email.value);
+// }
 
 
 
-const mouseEnterFun = (count) => {
-    if(count == 1)console.log("You entered the compound area");
+// const mouseEnterFun = (count) => {
+//     if(count == 1)console.log("You entered the compound area");
 
-    if(count == 2)console.log("You entered inside the temple");
+//     if(count == 2)console.log("You entered inside the temple");
 
-    if(count == 3)console.log("You are near the statue");
+//     if(count == 3)console.log("You are near the statue");
 
-    if(count == 4)console.log("You can escape from here");
-}
+//     if(count == 4)console.log("You can escape from here");
+// }
 
-const mouseOutFun = () => {
-    console.log("You escaped the temple");
-}
+// const mouseOutFun = () => {
+//     console.log("You escaped the temple");
+// }
 
+
+
+let childDivs = document.querySelectorAll(".child");
+
+
+var gameStarted = false;
+var resultCount;   
+let instructions = document.querySelector("#instructions");
+ 
+
+childDivs.forEach((child, id) => {
+    child.addEventListener('click', e => {
+        if(!gameStarted){
+            resultCount = Math.floor(Math.random() * 9);
+            console.log(resultCount);
+            gameStarted = true;
+            e.target.style.backgroundColor = "green";
+            instructions.innerText = "Find the matching box";
+        }else{
+            if(id === resultCount){
+                console.log('You win');
+                instructions.innerText = "You win";
+                instructions.style.backgroundColor = "green";
+                e.target.style.backgroundColor = "green";
+            }
+            else{
+                console.log('You loose');
+                instructions.innerText = "You loose";
+                instructions.style.backgroundColor = "red";
+                e.target.style.backgroundColor = "red";
+                childDivs[resultCount].style.backgroundColor = "green";
+            } 
+            gameStarted = false;
+            resultCount = -1;
+            
+            setTimeout(timeoutHandler, 2000 );
+        }
+
+    })
+})
+
+const timeoutHandler = () => location.reload();
